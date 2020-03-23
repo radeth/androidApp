21941eac3ce02950d74cd536763289fc2ca3de52
radas <123radeth@gmail.com>
2020-03-23 02:23:12 +0100

    hello firebase

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private val  database = Firebase.database
+    private val myRef = database.getReference("message")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myRef.setValue("Hello, World!")
     }
+
+
 }
+
+@IgnoreExtraProperties
+data class User(
+    var username: String? = "",
+    var email: String? = ""
+)
